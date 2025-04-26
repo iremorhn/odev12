cbbee4303503ee0fb497e9cdba8cbdbc34bb013b
iremorhn <iiremorhan@outlook.com>
2025-04-26 23:49:34 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/degiskenler/DegiskenOlusturma.kt
@@ -0,0 +1,141 @@
+package com.example.myapplication.degiskenler
+fun main() {
+    val dukkanSahibi = "dükkan sahibi"
+    println("Dükkan Sahibi: $dukkanSahibi")
+
+    val baharat = "baharat"
+    println("Baharat: $baharat")
+
+    val buyucu = "büyücü"
+    println("Büyücü: $buyucu")
+
+    val adliyeMubasiri = "adliye mübaşiri"
+    println("Adliye Mübaşiri: $adliyeMubasiri")
+
+    val kutuphaneMemuru = "kütüphane memuru"
+    println("Kütüphane Memuru: $kutuphaneMemuru")
+
+    val berber = "berber"
+    println("Berber: $berber")
+
+    val sofor = "şoför"
+    println("Şoför: $sofor")
+
+    val asci = "aşçı"
+    println("Aşçı: $asci")
+
+    val gazeteci = "gazeteci"
+    println("Gazeteci: $gazeteci")
+
+    val manken = "manken"
+    println("Manken: $manken")
+
+    val kadinSubay = "kadın subay"
+    println("Kadın Subay: $kadinSubay")
+
+    val isadami = "işadamı"
+    println("İşadamı: $isadami")
+
+    val iskadini = "işkadını"
+    println("İşkadını: $iskadini")
+
+    val ciftci = "çiftçi"
+    println("Çiftçi: $ciftci")
+
+    val yazar = "yazar"
+    println("Yazar: $yazar")
+
+    val muzikSefi = "müzik şefi"
+    println("Müzik Şefi: $muzikSefi")
+
+    val modaTasarmicisi = "moda tasarımcısı"
+    println("Moda Tasarımcısı: $modaTasarmicisi")
+
+    val terzi = "terzi"
+    println("Terzi: $terzi")
+
+    val kuafor = "kuaför"
+    println("Kuaför: $kuafor")
+
+    val sporcu = "sporcu"
+    println("Sporcu: $sporcu")
+
+    val insaatIscisi = "inşaat işçisi"
+    println("İnşaat İşçisi: $insaatIscisi")
+
+    val asciBasi = "aşçıbaşı"
+    println("Aşçıbaşı: $asciBasi")
+
+    val balikci = "balıkçı"
+    println("Balıkçı: $balikci")
+
+    val sirkMuduru = "sirk müdürü"
+    println("Sirk Müdürü: $sirkMuduru")
+
+    val aktor = "aktör"
+    println("Aktör: $aktor")
+
+    val aktris = "aktris"
+    println("Aktris: $aktris")
+
+    val bilimAdi = "bilim adamı"
+    println("Bilim Adamı: $bilimAdi")
+
+    val bilimKadini = "bilim kadını"
+    println("Bilim Kadını: $bilimKadini")
+
+    val hemsire = "hemşire"
+    println("Hemşire: $hemsire")
+
+    val doktor = "doktor"
+    println("Doktor: $doktor")
+
+    val polisMemuru = "polis memuru"
+    println("Polis Memuru: $polisMemuru")
+
+    val ogretmen = "öğretmen"
+    println("Öğretmen: $ogretmen")
+
+    val itfaiyeci = "itfaiyeci"
+    println("İtfaiyeci: $itfaiyeci")
+
+    val sarkici = "şarkıcı"
+    println("Şarkıcı: $sarkici")
+
+    val dansci = "dansçı"
+    println("Dansçı: $dansci")
+
+    val bankaci = "bankacı"
+    println("Bankacı: $bankaci")
+
+    val hakim = "hakim"
+    println("Hakim: $hakim")
+
+    val subay = "subay"
+    println("Subay: $subay")
+
+    val hemsireYardimcisi = "hemşireyardımcısı"
+    println("Hemşire Yardımcısı: $hemsireYardimcisi")
+
+    val temizlikci = "temizlikçi"
+    println("Temizlikçi: $temizlikci")
+
+    val mimar = "mimar"
+    println("Mimar: $mimar")
+
+    val cocukBakicisi = "çocuk bakıcısı"
+    println("Çocuk Bakıcısı: $cocukBakicisi")
+
+    val asker = "asker"
+    println("Asker: $asker")
+
+    val taksiSoforu = "taksi şoförü"
+    println("Taksi Şoförü: $taksiSoforu")
+
+    val veteriner = "veteriner"
+    println("Veteriner: $veteriner")
+
+    val avukat = "avukat"
+    println("Avukat: $avukat")
+}
+
